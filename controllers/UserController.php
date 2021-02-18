b2c475b2bd000e95657bc25723b9b8b8c114b713
JuanSanz2000 <49302792+JuanSanz2000@users.noreply.github.com>
2021-02-18 15:37:56 +0100

    intento_de_auth

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -12,22 +12,22 @@ class UserController extends Controller
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // Si se envían los datos en formato raw dentro de la petición http, se recogen así:
-            /*
-            $params = json_decode(file_get_contents("php://input"), false);
-            @$username = $params->username;
-            @$password = $params->password;
-            */
-            // Si se envían los datos de la forma habitual (form-data), se reciben en $_POST:
-            $username = $_POST['username'];
-            $password = $_POST['password'];
 
-            if ($u = \app\models\Usuarios::findOne(['usuario' => $username]))
+            $params = json_decode(file_get_contents("php://input"), false);
+            @$usuario = $params->usuario;
+            @$password = $params->password;
+
+            // Si se envían los datos de la forma habitual (form-data), se reciben en $_POST:
+            // $usuario = $_POST['usuario'];
+            // $password = $_POST['password'];
+
+            if ($u = \app\models\Usuarios::findOne(['usuario' => $usuario]))
                 if ($u->password == md5($password)) { //o crypt, según esté en la BD
 
                     return ['token' => $u->token, 'id' => $u->id, 'nombre' => $u->nombre];
                 }
 
-            return ['error' => 'Usuario incorrecto. ' . $username];
+            return ['error' => 'Usuario incorrecto. ' . $usuario];
         }
     }
 }
