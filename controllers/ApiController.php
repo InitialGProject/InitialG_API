6e0693ec54091ebc68cb2f2fac09a9e0843767f5
Marta Pretel <mpretel3@iesfuentesanluis.org>
2021-02-22 23:29:52 +0100

    behavior fuera

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -20,16 +20,7 @@ class ApiController extends \yii\rest\ActiveController
     {
         $behaviors = parent::behaviors();
         unset($behaviors['authenticator']);
-        $behaviors['corsFilter'] = [
-            'class' => Cors::className(),
-            'cors' => [
-                'Origin' => ['*'],
-                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-                'Access-Control-Request-Headers' => ['*'],
-                'Access-Control-Allow-Credentials' => $this->authenable,
-                'Access-Control-Max-Age' => 86400
-            ],
-        ];
+       
 
         if (!$this->authenable)
             return $behaviors;
--- web/index.php
+++ web/index.php
@@ -3,6 +3,9 @@
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
