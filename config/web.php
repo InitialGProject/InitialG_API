516d82e73f4947877cee1eb149f5d45d3ab40423
mithjack <alexlopez168@hotmail.com>
2021-02-22 14:39:12 +0100

    fix token

--- config/web.php
+++ config/web.php
@@ -22,7 +22,7 @@ $config = [
             'class' => 'yii\caching\FileCache',
         ],
         'user' => [
-            'identityClass' => 'app\models\User',
+            'identityClass' => 'app\models\Usuarios',
             'enableAutoLogin' => true,
         ],
         'errorHandler' => [
